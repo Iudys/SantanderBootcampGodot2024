9eb4dae4d06eb7c1e369c0c8a4ce3eca0d600122
Iuri Guimaraes Guerra <68698094+Iudys@users.noreply.github.com>
2024-05-21 17:06:44 -0300

    TinySwords Starts

---
+++ TinySwords/Script/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var is_running: bool = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("move_up"):
+		if is_running:
+			animation_player.play("Iddle")
+			is_running = false
+		else:
+			animation_player.play("Walking")
+			is_running = true
+	if Input.is_action_just_pressed("move_down"):
+		print("Eu gosto muthu")
+	
